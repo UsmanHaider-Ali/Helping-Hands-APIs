0fd6cdc1c54726682c44237ebbf46614b7444bc0
Usman Haider Ali <UsmanHaider_Ali@outlook.com>
2022-10-18 14:40:32 +0500

    Trying to fix mongoose error

--- apis/databases/database-configuration.js
+++ apis/databases/database-configuration.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 require("dotenv").config();
+dotenv.config();
 
 exports.databaseConnection = () => {
   try {
+    console.log(process.env.DATABASE_CONNECTION_KEY);
     mongoose.connect(process.env.DATABASE_CONNECTION_KEY);
     console.log("Database connected successfully.");
   } catch (error) {
